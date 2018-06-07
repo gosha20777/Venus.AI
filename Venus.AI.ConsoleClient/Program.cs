596cee0da9fec84a3c983ab5a4587fbfdfa418a3
Georgy Perevozchikov <gosha20777@live.ru>
2018-06-07 12:53:35 +0300

    add-bot-and-console-lient

---
+++ Venus.AI.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Venus.AI.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
