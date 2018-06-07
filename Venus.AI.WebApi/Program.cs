b162b6c804ec4eede949f9241e55bba80d5cfb4f
Georgy Perevozchikov <gosha20777@live.ru>
2018-06-07 12:44:57 +0300

    initial-commit

--- Venus.AI.WebApi/Program.cs
+++ Venus.AI.WebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Venus.AI
+namespace Venus.AI.WebApi
 {
     public class Program
     {
--- Venus.AI.WebApi/Startup.cs
+++ Venus.AI.WebApi/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Venus.AI
+namespace Venus.AI.WebApi
 {
     public class Startup
     {
